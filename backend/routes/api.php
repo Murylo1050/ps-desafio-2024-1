4b45e1e98d9c2a5cfed6877db6bb9fd5a33975cd
murylo <murylo.diasolirub@gmail.com>
2024-05-09 03:35:13 -0300

    store de categorias com erro

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,10 +1,13 @@
 <?php
 
+use App\Http\Controllers\CategoriaController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Symfony\Component\HttpFoundation\Response;
+
+Route::apiResource('categories', CategoriaController::class);
 
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/profile', function (Request $request) {
