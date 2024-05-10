d026ebe2bf66c19cb8b387898b539b6909218d24
murylo <murylo.diasolirub@gmail.com>
2024-05-10 01:51:49 -0300

    teste nome da tabela ingles

--- backend/app/Http/Requests/UpdateProductRequest.php
+++ backend/app/Http/Requests/UpdateProductRequest.php
@@ -11,7 +11,7 @@ class UpdateProductRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,11 @@ class UpdateProductRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'sometimes|min:3|max:80',
+            'price' => 'sometimes',
+            'amount' => 'sometimes',
+            'image' => 'sometimes',
+            'category_id' => 'sometimes',
         ];
     }
 }
