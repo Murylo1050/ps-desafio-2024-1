ca7dba1d278ee5ce4f6f50bebbc99a306329c4dc
murylo <murylo.diasolirub@gmail.com>
2024-05-25 21:05:22 -0300

    responsividade feita, filtros por nome e categoria feitos

--- frontend/src/app/(site)/page.tsx
+++ frontend/src/app/(site)/page.tsx
@@ -6,13 +6,21 @@ import { useEffect, useState } from 'react'
 import Card from '@/components/site/cards/card'
 import style from './syle.module.css'
 import Navbar from '@/components/site/navbar/navbar'
+import { categoryType } from '@/types/category'
 
 export default function Home() {
   const [products, setProduct] = useState<productType[] | null>(null)
+  const [category, setCategory] = useState<categoryType[] | null>(null)
+  const [search, setSearch] = useState<string>('')
+  const [selectedCategory, setSelectCategory] = useState<string>('')
+
   const requestData = async () => {
     try {
-      const response: productType[] = await api.get('/products')
-      setProduct(response)
+      const responseProduct: productType[] = await api.get('/products')
+      setProduct(responseProduct)
+
+      const responseCategory: categoryType[] = await api.get('/categories')
+      setCategory(responseCategory)
     } catch (e) {
       console.log(e)
     }
@@ -25,10 +33,44 @@ export default function Home() {
   return (
     <div className={style.productPage}>
       <Navbar logo="/images/logoAdapti.png" />
+      <div className={style.filter_div}>
+        <input
+          type="text"
+          className={style.filter_input_text}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <select
+          name=""
+          className={style.filter_select}
+          id=""
+          onChange={(selected) => setSelectCategory(selected.target.value)}
+        >
+          <option value="" className={style.option}>
+            Sem Filtro
+          </option>
+
+          {category?.map((category: categoryType, index: number) => (
+            <option value={category.name} key={index} className={style.option}>
+              {category.name}
+            </option>
+          ))}
+        </select>
+      </div>
       <div className={style.wrapper}>
-        {products?.map((product: productType, index: number) => (
-          <Card product={product} key={index} />
-        ))}
+        {products
+          ?.filter((product) => {
+            return selectedCategory === ''
+              ? product
+              : product.category.name.includes(selectedCategory)
+          })
+          .filter((product) => {
+            return search.toLowerCase() === ''
+              ? product
+              : product.name.toLowerCase().includes(search)
+          })
+          .map((product: productType, index: number) => (
+            <Card product={product} key={index} />
+          ))}
       </div>
     </div>
   )
--- frontend/src/components/site/navbar/navbar.tsx
+++ frontend/src/components/site/navbar/navbar.tsx
@@ -3,8 +3,6 @@
 import style from './style.module.css'
 import { useEffect, useState } from 'react'
 import { getSession } from 'next-auth/react'
-import { categoryType } from '@/types/category'
-import { api } from '@/services/api'
 
 interface NavProps {
   logo: string
@@ -12,7 +10,6 @@ interface NavProps {
 
 export default function Navbar({ logo }: NavProps) {
   const [isAuth, setIsAuth] = useState<boolean>(false)
-  const [category, setCategory] = useState<categoryType[] | null>(null)
   const requestDataSession = async () => {
     try {
       const sessionReponse = await getSession()
@@ -21,18 +18,9 @@ export default function Navbar({ logo }: NavProps) {
       console.log(e)
     }
   }
-  const requestData = async () => {
-    try {
-      const response: categoryType[] = await api.get('/products')
-      setCategory(response)
-    } catch (e) {
-      console.log(e)
-    }
-  }
 
   useEffect(() => {
     requestDataSession()
-    requestData()
   }, [])
   return (
     <nav className={style.navbar}>
@@ -46,20 +34,6 @@ export default function Navbar({ logo }: NavProps) {
             <a href="" className={style.nav_button}>
               Inicio
             </a>
-          </li>
-          <li className={style.nav_item}>
-            <a href="" className={style.nav_button}>
-              Categorias
-            </a>
-            <ul className={style.dropdown}>
-              {category?.map((category: categoryType, index: number) => [
-                <li className={style.dropdown_item} key={index}>
-                  <a href="" className={style.dropdown_button}>
-                    {category.name}
-                  </a>
-                </li>,
-              ])}
-            </ul>
           </li>
 
           <li className={style.nav_item}>
